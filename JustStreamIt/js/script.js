2a52fe8130abbea7c8ec330a3cf9fefc3c3fa356
Gaiden92 <samy_fouchal@hotmail.fr>
2023-09-26 21:35:37 +0200

    - création div "best movie" - création fonction get_best_movie_data - modification fonction getDataByGenre

--- JustStreamIt/js/script.js
+++ JustStreamIt/js/script.js
@@ -5,39 +5,95 @@ const adventure = "Adventure";
 
 const url = "http://127.0.0.1:8000/api/v1/titles/";
 
-function getDataByGenre(genre = "All") {
+// fonction affichage bloc "meilleur film"
+function get_best_movie_data(requete_best_movie){
+    fetch(requete_best_movie).then(response => response.json().then(data => {
+                    
+        title_movie = data["title"];
+        description_movie = data["description"];
+        image_movie_src = data["image_url"];
+
+        h2 = document.createElement("h2");
+        h2.innerHTML = title_movie;
+
+        p = document.createElement("p");
+        p.innerHTML = description_movie
+
+        img = document.createElement("img");
+        img.src = image_movie_src
+
+        // Div meilleur film
+        document.querySelector("#best-movie").appendChild(h2);
+        document.querySelector("#best-movie").appendChild(p);
+        document.querySelector("#best-movie").appendChild(img);
+    }))
+}
+
+async function getDataByGenre(genre = "All") {
     let nb_page = "";
     let request;
     let results = []
 
+    //On boucle de la page 1 à 2
+    for(let i=1;i <= 2;i++){
+        if (genre === "All") {
 
-    if (genre === "All") {
-        request = fetch(url + "?sort_by=-imdb_score" + nb_page);
-        } else {
-        request = fetch(url + "?genre=" + genre + "&sort_by=-imdb_score"+ nb_page)
-        }
-        request.then(response => response.json()).then(data =>
-    
+        
+            if (i == 2){
+                nb_page = "&page=2";
+            }
+            request = fetch(url + "?sort_by=-imdb_score" + nb_page);
+            }
+        else {
+            if (i == 2){
+                nb_page = "&page=2";
+            }
+            request = fetch(url + "?genre=" + genre + "&sort_by=-imdb_score"+ nb_page)
+            }
+        
+        await request.then(response => response.json()).then(data =>
+        
         {
-            results = data["results"];
-            console.log(results);
+            // On récupère les données "résultats" pour les pages 1 et 2. 
+            let data_by_page = data["results"];
+            if (i == 2){
+                //Si nous sommes sur la page 2, on récupère seulement les 2 premiers résultats.
+                data_by_page = data["results"].slice(0,2);
+                
+            } 
+           
+            // On affecte à notre variable "results" le tableau de donnée récupéré. 
+            results = data_by_page;
+
             let genre_str = genre.toLowerCase();
-    
+            
+            // Requete pour récuperer les informations du meilleur film
+            if (genre_str == "all" && i == 1){
+                let best_movie_data = results[0];
+                request = best_movie_data["url"];
+                get_best_movie_data(request);
+            }
+
+            
+            // Création d'un h3 et d'une balise img pour chaque élement (film) de notre tableau.
             results.forEach(element => {
     
                 //Titre
                 h3 = document.createElement("h3");
                 h3.innerHTML = element["title"];
-    
-                // Image
+                
+
+                //Image
                 img = document.createElement("img");
                 img.src = element["image_url"];
                 document.querySelector("#best-" + genre_str + "-movies").appendChild(img);
-    
                 document.querySelector(".container-" + genre_str + "").appendChild(img);
-    
+                
+                
             });
         })
+    }
+    
     
    
 
